e3fbfa02e1d599c45f29f8b6121129aaabb82f29
nihatran <89045243+nihatran@users.noreply.github.com>
2023-12-28 20:58:57 -0500

    Create basic layout

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, num1, num2) {
+     return operator(num1, num2);
+}
+
+let num1;
+let operator;
+let num2;
