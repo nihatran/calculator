41a4c212f27a519bb07e0817e66aaeb29ba2f5c7
nihatran <89045243+nihatran@users.noreply.github.com>
2023-12-29 22:44:29 -0500

    Add number display without overflow

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 
+let num1;
+let operator;
+let num2;
+
+let numButtons = document.querySelectorAll('.btn-num');
+let operatorButtons = document.querySelectorAll('.btn-operator');
+let equalsButton = document.querySelector('.btn-equals');
+let screenCurrent = document.querySelector('.screen-current');
+let screenLast = document.querySelector('.screen-last');
+
+
+function assignNumButtons() {
+    numButtons.forEach((button) => {
+        button.addEventListener('click', () => displayNum(button.textContent))
+    })
+}
+
+
+function displayNum(number) {
+    screenCurrent.textContent += number;
+}
+
+
+
+
+
+
+
+
 function add(a, b) {
     return a + b;
 }
@@ -19,6 +48,11 @@ function operate(operator, num1, num2) {
      return operator(num1, num2);
 }
 
-let num1;
-let operator;
-let num2;
+
+assignNumButtons();
+
+
+
+
+
+// add event onclick to display the value it is 
