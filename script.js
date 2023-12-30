411a5c516caa57c916eafc824de5c945a3f27cdf
nihatran <89045243+nihatran@users.noreply.github.com>
2023-12-30 00:06:58 -0500

    Get numbers to display on the screen

--- script.js
+++ script.js
@@ -3,26 +3,56 @@ let num1;
 let operator;
 let num2;
 
+
+
 let numButtons = document.querySelectorAll('.btn-num');
 let operatorButtons = document.querySelectorAll('.btn-operator');
 let equalsButton = document.querySelector('.btn-equals');
 let screenCurrent = document.querySelector('.screen-current');
 let screenLast = document.querySelector('.screen-last');
 
+let operatorSelected = false;
 
-function assignNumButtons() {
+
+function getNumButtons() {
     numButtons.forEach((button) => {
-        button.addEventListener('click', () => displayNum(button.textContent))
+        button.addEventListener('click', () => setNum(button.textContent))
     })
 }
 
+function getOperatorButtons() {
+    operatorButtons.forEach((button) => {
+        button.addEventListener('click', () => setOperator(button.textContent))
+    })
+}
 
-function displayNum(number) {
-    screenCurrent.textContent += number;
+function setNum(number) {
+    if (operatorSelected === false) {
+        screenCurrent.textContent += number;
+        num1 = screenCurrent.textContent;
+    } else {
+        screenCurrent.textContent += number;
+        
+    }
+}
+
+function setOperator(op) {
+    if (operatorSelected === false) {
+        screenLast.textContent = screenCurrent.textContent + ' ' + op;
+        operator = op;
+        screenCurrent.textContent = '';
+        operatorSelected = true;
+        calculate();
+    }
 }
 
 
-
+function calculate() {
+    equalsButton.addEventListener('click', () => {
+        num2 = screenCurrent.textContent;
+        console.log(num1, num2, operator);
+    })
+} 
 
 
 
@@ -49,10 +79,8 @@ function operate(operator, num1, num2) {
 }
 
 
-assignNumButtons();
+getNumButtons();
+getOperatorButtons();
 
 
-
-
-
-// add event onclick to display the value it is 
+// use switch statement for operate function
